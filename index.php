d9e5acfea26e45d85c6ceef02a9b07234fc340c0
Ian <ninzheruela@gmail.com>
2024-04-04 19:56:08 +0800

    Portfolio

---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/hamburgers.min.css">
+    <title>Porfolio</title>
+</head>
+<body>
+    <div class="clearfix">
+    <div class="header">
+        <div class="wrapper">
+            <div class="header_con">
+                <div class="main_logo">
+                    <a href="">
+                        <figure><img src="images/main-logo.png" alt=""></figure>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--navigation-->
+<!-- Navigation -->
+<div id="nav_area">
+    <div class="nav_toggle_button">
+        <div class="logo_wrap"></div>
+        <div class="toggle_holder">
+            <div class="hamburger hamburger--spin-r">
+                <div class="hamburger-box">
+                    <div class="hamburger-inner"></div>
+                </div>
+            </div>
+            <small>Menu</small>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+
+    <div class="toggle_right_nav">
+        <div class="page_nav_locator"></div>
+        <nav class="page_nav">
+            <div class="menu_slide_right">
+                <a href="#" class="logo_slide_right">
+                    <figure><img src="images/main-logo.png" alt="" /></figure>
+                </a>
+                <div class="toggle_holder">
+                    <div class="hamburger hamburger--spin-r">
+                        <div class="hamburger-box">
+                            <div class="hamburger-inner"></div>
+                        </div>
+                    </div>
+                    <small>Close</small>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+
+            <div class="wrapper">
+                <!-- <!?php wp_nav_menu( array( 'container_class' => 'nav-menu', 'theme_location' => 'primary', 'after' => '<span><i class="fa fa-2x">&nbsp;&nbsp;&nbsp;&nbsp;</i></span>') ); ?> -->
+                <ul>
+                    <li class="current_page_item"><a href="#nav_area">Home</a></li>
+                    <li><a href="#middle">Portolio</a></li>
+                    <li><a href="#bottom">Resume</a></li>
+                    <li><a href="#bottom">About Me</a></li>
+                    <li><a href="#bottom3">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="toggle_nav_close"></div>
+    </div>
+</div>
+
+<div id="middle">
+    <div class="wrapper">
+        <div class="middle_con">
+            <div class="middle_info">
+                <h1><small>Portfolio</small>Cris Marie Leoligao</h1>
+            </div>
+            <div class="middle_home">
+                <div class="left_section">
+                   <section class="middle_boxes1">
+                     <h2>Proposed 2nd Floor Renovation</h2>
+                    <figure><img src="images/main_area/1.png" alt=""></figure>
+                    <figure><img src="images/main_area/2.png" alt=""></figure>
+                    <figure><img src="images/main_area/3.png" alt=""></figure>
+                    <figure><img src="images/main_area/4.png" alt=""></figure>
+                   </section>
+                </div>
+
+                 <div class="right_section">
+                   <section class="middle_boxes2">
+                     <h2>Proposed Interior Fit-Out</h2>
+                    <figure><img src="images/main_area/5.png" alt=""></figure>
+                    <figure><img src="images/main_area/6.png" alt=""></figure>
+                   </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="main_area">
+    <div class="wrapper">
+        <div class="main_area_con">
+             <div class="main_area_info">
+                    <h1>Education</h1>
+                </div>
+            <div class="main_area_boxes">
+                <div class="left_box">
+                     <section class="main_box1">
+                        <small>Secondary</small>
+                        <span>2011 - 2015</span>
+                        <p>University of the Visayas Minglanilla Campus</p>
+                    </section>
+                </div>
+                 <div class="right_box">
+                    <section class="main_box2">
+                        <small>Tertiary</small>
+                        <h2>Bachelor of Science in Architecture <span>2015 - 2020</span></h2>
+                        <p>Cebu Institute of Technology - University</p>
+                    </section>
+                </div>
+            </div>
+
+             <div class="main_area_info2">
+                    <h1>Experience</h1>
+                </div>
+
+            <div class="main_area_box2">
+
+            <div class="first_box">
+                <section class="main_box3">
+                    <h2>JMEA ARCHITECTS <span>Architectural Apprentice</span></h2>
+                    <ul>
+                        <li>Drafted detailed information on design, architectural specifications, materials, and cost of the building.</li>
+                    </ul>
+                </section>
+
+                <section class="main_box4">
+                     <h2>GONZALES VERTICAL HORIZON DESIGNS <span>Architectural Apprentice</span></h2>
+                    <ul>
+                        <li>Site supervision</li>
+                        <li>Manage workers on site</li>
+                        <li>Canvassing and purchasing materials needed for the site</li>
+                    </ul>
+                </section>
+            </div>
+
+            <div class="second_box">
+                   <section class="main_box5">
+                   <h2>KJ CONSTRAK COMPANY <span>Architectural Apprentice</span></h2>
+                    <ul>
+                        <li>Site Visit</li>
+                        <li>Drafted detailed information on design, architectural specifications, materials, and cost of the building.</li>
+                    </ul>
+                </section>
+
+                <section class="main_box6">
+                    <h2>EG ARCHITECTS <span>Architectural Apprentice</span></h2>
+                    <ul>
+                        <li>Drafted detailed information on design, architectural specifications, materials, and cost of the building.</li>
+                        <li>Canvassing and purchasing materials for the construction on site</li>
+                        <li>Meet and discuss architectural projects with clients</li>
+                        <li>Site visit</li>
+                    </ul>
+                </section>  
+            </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+    <div id="bottom">
+        <div class="wrapper">
+            <div class="bottom_con">
+                <figure><img src="images/me.png" alt=""></figure>
+                <div class="bottom_info">
+                <h1>About <strong>Me</strong></h1>
+                <p>Seeking to use my knowledge to benefit the team and have this opportunity to learn and gain more understanding about architecture.</p>
+                <a href="">Contact Me</a>
+                <a href="">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="bottom2">
+        <div class="wrapper">
+            <div class="btm2_con">
+                <div class="btm2_info">
+                    <h1>My Skills</h1>
+                </div>
+                <div class="bar_box">
+                <div class="skills_con">
+                <div class="bar_con">
+                <div class="progress-wrap">
+                        <h3>AutoCad</h3>
+                        <div class="progress">
+                            <div class="progress-bar color-1" style="width:90%">
+                                <span>90%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-wrap">
+                        <h3>Sketch Up</h3>
+                        <div class="progress">
+                            <div class="progress-bar color-2" style="width:90%">
+                                <span>90%</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="bar_con2">
+                    <div class="progress-wrap">
+                        <h3>Rendering</h3>
+                        <div class="progress">
+                            <div class="progress-bar color-3" style="width:80%">
+                                <span>80%</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="progress-wrap">
+                        <h3>Photoshop</h3>
+                        <div class="progress">
+                            <div class="progress-bar color-4" style="width:60%">
+                                <span>60%</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div> 
+
+                <div class="skill_con2">
+                     <div class="bar_con3">
+                    <div class="progress-wrap">
+                        <h3>Microsoft Excel</h3>
+                        <div class="progress">
+                            <div class="progress-bar color-5" style="width:90%">
+                                <span>90%</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+
+    <div id="bottom3">
+        <div class="wrapper">
+            <div class="btm3_con">
+                  <div class="btm_form_info">
+                        <h2>Get In Touch</h2>
+                        <h2>My Contact Details</h2>
+                    </div>
+                <div class="btm_form">
+                  
+                    <form action="">
+                    
+                        <input type="text" name="" class="" placeholder="Your Name" required>
+                        <input type="email" name="" class="" placeholder="Your Email" required>
+                        <textarea name="" id="" placeholder="Message"></textarea>
+                        <input type="submit" name="" class="" value="Submit">
+                    </form>
+                
+                <div class="contact_details">
+                    
+                    <ul>
+                        <li><span>Email</span>crismarieleoligao95@gmail.com</li>
+                        <li><span>Phone</span>(+63) 915 657 2642</li>
+                        <li><span>Address</span>Minglanilla, Cebu </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="wrapper">
+            <div class="footer_con">
+                <div class="copy_right">
+                    <p>Copyright &copy; 2024 All rights reserved </p>
+                </div>
+                <div class="social_icons">
+                    <a href="https://www.facebook.com/" target="_blank"><figure><img src="images/fb.png" alt=""></figure></a>
+                    <a href="https://www.instagram.com/" target="_blank"><figure><img src="images/insta.png" alt=""></figure></a>
+                    <a href="https://www.linkedin.com/login" target="_blank"><figure><img src="images/linkedin.png" alt=""></figure></a>
+                    <a href="https://twitter.com/?lang=en" target="_blank"><figure><img src="images/X.png" alt=""></figure></a>
+                    <!-- <figure><img src="images/fb.png" alt=""></figure> -->
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    
+
+    </div>
+    <script src="js/calcheight.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.skitter.min.js"></script>
+    <script src="js/responsiveslides.min.js"></script>
+    <script src="js/css3-animate-it.min.js"></script>
+    <script src="js/plugins.min.js"></script>
+    <script src="js/wow.min.js"></script>
+</body>
+</html>
